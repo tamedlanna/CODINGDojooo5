65b9930e9ebf9cd0e98629bc8a071793db6637f1
Anna Tamedl <wi16b055@technikum-wien.at>
2018-01-11 01:21:23 +0100

    update of CD5 -.-

---
+++ CODINGDojooo5/ViewModel/ItemVm.cs
@@ -0,0 +1,52 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CODINGDojooo5.ViewModel
+{
+    public class ItemVm : ViewModelBase
+    {
+        public string Desription { get; set; }
+        public string AgeRecom { get; set; }
+        public BitmapImage Image { get; set; }
+
+        public ObservableCollection<ItemVm> Items { get; set; }
+
+       
+
+        public int NoOfParts
+        {
+            get {
+                if (Items != null)
+                {
+                    return Items.Count;
+                }
+                else {
+                    return 0;
+                }
+            }
+        }
+
+        public ItemVm(string description, BitmapImage image, string agerecom) {
+            Desription = description;
+            
+            Image = image;
+            AgeRecom = agerecom;
+        }
+
+        public void AddItem(ItemVm item) {
+            if (Items == null) {
+                Items = new ObservableCollection<ItemVm>();
+            }
+
+            Items.Add(item);
+        }
+
+    }
+}
